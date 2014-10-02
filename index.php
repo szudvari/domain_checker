0134f32a62bb360a820e5eba163a83f66e3a1b4c
Szabolcs Udvari <udvarisz@yahoo.com>
2014-10-02 10:54:24 +0200

    css

--- index.php
+++ index.php
@@ -9,6 +9,17 @@
 
 <!-- Required header files -->
 <link href="css/style.css" rel="stylesheet" type="text/css">
+<link rel='stylesheet' id='css3_grid_table1_style-css'  href='http://www.webariel.hu/wp-content/plugins/css3_web_pricing_tables_grids/table1/css3_grid_style.css?ver=4.0' type='text/css' media='all' />
+<link rel='stylesheet' id='css3_grid_table2_style-css'  href='http://www.webariel.hu/wp-content/plugins/css3_web_pricing_tables_grids/table2/css3_grid_style.css?ver=4.0' type='text/css' media='all' />
+<link rel='stylesheet' id='css3_grid_responsive-css'  href='http://www.webariel.hu/wp-content/plugins/css3_web_pricing_tables_grids/responsive.css?ver=4.0' type='text/css' media='all' />
+<link rel='stylesheet' id='divi-fonts-css'  href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,700italic,800italic,400,300,700,800&#038;subset=latin,latin-ext' type='text/css' media='all' />
+<link rel='stylesheet' id='divi-style-css'  href='http://www.webariel.hu/wp-content/themes/webariel/style.css?ver=4.0' type='text/css' media='all' />
+<link rel='stylesheet' id='visual-form-builder-css-css'  href='http://www.webariel.hu/wp-content/plugins/visual-form-builder/css/visual-form-builder.min.css?ver=20140412' type='text/css' media='all' />
+<link rel='stylesheet' id='vfb-custom-css-css'  href='http://www.webariel.hu/wp-content/themes/webariel/css/vfb-custom-css.css?ver=4.0' type='text/css' media='all' />
+<link rel='stylesheet' id='et-shortcodes-css-css'  href='http://www.webariel.hu/wp-content/themes/webariel/epanel/shortcodes/css/shortcodes.css?ver=3.0' type='text/css' media='all' />
+<link rel='stylesheet' id='et-shortcodes-responsive-css-css'  href='http://www.webariel.hu/wp-content/themes/webariel/epanel/shortcodes/css/shortcodes_responsive.css?ver=3.0' type='text/css' media='all' />
+<link rel='stylesheet' id='footable-core-min-css'  href='http://www.webariel.hu/wp-content/plugins/footable/css/footable.core.min.css?ver=0.3.1' type='text/css' media='all' />
+<link rel='stylesheet' id='footable-metro-min-css'  href='http://www.webariel.hu/wp-content/plugins/footable/css/footable.metro.min.css?ver=0.3.1' type='text/css' media='all' />
 <script type="text/javascript" src="js/jquery_1.5.2.js"></script>
 <script type="text/javascript" src="js/post_watermarkinput.js"></script>
 <script type="text/javascript" src="js/vpb_script.js"></script>
@@ -19,7 +30,8 @@
 </head>
 <body>
 <br clear="all">
-<h2>Domai név keresése</h2>
+<center>
+<div style="font-size:24px;">Domain név kereső</div><br clear="all" /><br clear="all" />
 
 <!-- Code Begins -->
 <center>
